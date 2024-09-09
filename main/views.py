ee07412fbf8a7b405a1a0ba8610d1df193fcdf67
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2024-09-09 15:12:06 +0700

    added procfile

--- main/views.py
+++ main/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'EsQua',
-        'price': '5000',
-        'stock': '10',
-        'description':'Minuman Segerrrrrrrrrrrr'
+        'name': 'EsQuaaaa',
+        'class' : 'PBP',
+        'price' : 5000,
+        'stock' : 10,
+        'description': 'Minuman Segerrrrrrrrrrrr'
     }
 
     return render(request, "main.html", context)
